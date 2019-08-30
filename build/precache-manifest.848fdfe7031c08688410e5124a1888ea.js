d906e1db02c7af5bf21fb2d8ba5682235c25a996
Brad Anderson <banderso@nerdery.com>
2019-08-30 13:36:14 -0400

    Quick and dirty deploy

---
+++ build/precache-manifest.848fdfe7031c08688410e5124a1888ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c1a4c12ad751bdb7a9a6f22f3d725b",
+    "url": "/ducks/build/index.html"
+  },
+  {
+    "revision": "cbf5df9f7bca0adb243a",
+    "url": "/ducks/build/static/css/main.720834b4.chunk.css"
+  },
+  {
+    "revision": "9eabe8e19e07011d7b72",
+    "url": "/ducks/build/static/js/2.0d001cb9.chunk.js"
+  },
+  {
+    "revision": "cbf5df9f7bca0adb243a",
+    "url": "/ducks/build/static/js/main.331422a5.chunk.js"
+  },
+  {
+    "revision": "a104d99bc1192f8a488a",
+    "url": "/ducks/build/static/js/runtime~main.86ad17e6.js"
+  }
+]);
